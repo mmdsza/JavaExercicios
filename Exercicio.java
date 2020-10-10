054d84a5c44ffc7305b78d253d8578aa775588d3
Mauricio de Souza <mmdsza@outlook.com>
2020-10-10 11:48:26 -0700

    Trying new things

--- Exercicio.java
+++ Exercicio.java
@@ -9,9 +9,23 @@ public class Exercicio{
 
         // Construtor da classe pessoa
 
-        Pessoa(String nome, int idade, float salario){
+        Pessoa(String nome, int idade){
             this.nome = nome;
             this.idade = idade;
+            //this.salario = salario;
+        }
+
+
+    }
+    public static class Funcionario extends Pessoa{
+        String cargo;
+        float salario;
+        String nome;
+        int idade;
+
+
+        Funcionario(String cargo, float salario, String nome, int idade){
+            this.cargo = cargo;
             this.salario = salario;
         }
 
@@ -19,10 +33,11 @@ public class Exercicio{
     public static void main(String[] args) {
         Scanner myObj = new Scanner(System.in);
         System.out.println("Digite seu nome, idade e salario, por favor.");
-        Pessoa pessoa1 = new Pessoa(myObj.nextLine(), myObj.nextInt(), myObj.nextFloat());
+        Pessoa pessoa1 = new Pessoa(myObj.nextLine(), myObj.nextInt());
+        Funcionario funcionario1 = new Funcionario(myObj.nextLine(), myObj.nextFloat());
         System.out.println("Ola, "+pessoa1.nome);
         System.out.println("Sua idade e: "+pessoa1.idade);
-        System.out.println("Seu salario: "+ pessoa1.salario);
+        System.out.println("Seu salario: "+ funcionario1.salario);
 
         //System.out.println("Informe seu nome, idade e salario");
     }

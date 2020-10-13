a19444f9c96b46061a1e0d52040ec3647113cd18
Mauricio de Souza <mmdsza@outlook.com>
2020-10-13 09:18:48 -0700

    pfvr funfa

--- Exercicio.java
+++ Exercicio.java
@@ -5,11 +5,11 @@ public class Exercicio{
         // estrutura da classe pessoa
         String nome;
         int idade;
-        float salario;
+        //float salario;
 
         // Construtor da classe pessoa
 
-        Pessoa(String nome, int idade){
+        public Pessoa(String nome, int idade){
             this.nome = nome;
             this.idade = idade;
             //this.salario = salario;
@@ -19,25 +19,26 @@ public class Exercicio{
     }
     public static class Funcionario extends Pessoa{
         String cargo;
-        float salario;
-        String nome;
-        int idade;
+        int salario;
+        //String nome;
+        //int idade;
 
 
-        Funcionario(String cargo, float salario, String nome, int idade){
+        public Funcionario(String cargo, int salario){
             this.cargo = cargo;
             this.salario = salario;
+            //this.nome = nome;
+            //this.idade = idade;
         }
-
     }
     public static void main(String[] args) {
         Scanner myObj = new Scanner(System.in);
         System.out.println("Digite seu nome, idade e salario, por favor.");
         Pessoa pessoa1 = new Pessoa(myObj.nextLine(), myObj.nextInt());
-        Funcionario funcionario1 = new Funcionario(myObj.nextLine(), myObj.nextFloat());
+        Funcionario funcionario1 = new Funcionario(myObj.nextLine(), myObj.int());
         System.out.println("Ola, "+pessoa1.nome);
         System.out.println("Sua idade e: "+pessoa1.idade);
-        System.out.println("Seu salario: "+ funcionario1.salario);
+        System.out.println("Seu salario: "+ funcionario.salario);
 
         //System.out.println("Informe seu nome, idade e salario");
     }
